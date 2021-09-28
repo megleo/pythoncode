7e1e3d64efa8b96078378e173ce54d063d7494e1
ml3082298291 <3082298291@qq.com>
2021-09-28 20:45:31 +0800

    5 python的数据类型

---
+++ 2python的数据类型.py
@@ -0,0 +1,20 @@
+# -*- codeing = utf-8 -*-
+# @Auther : menglei 2021180020
+# @Filer : 2python的数据类型.py
+# @Software : PyCharm
+# @Time ： 2021/9/28 8:30 下午
+
+# python的数据类型
+
+# 什么是数据类型
+
+#常用的数据类型
+# 在python中提供了type函数用户获得当前变量的类型。 type（）
+# 1 字符串类型
+love = 'iloveyou'
+hello = 'helloworld'
+like = "i love u"
+print(love,hello,like)
+print(love,type(love))
+
+
